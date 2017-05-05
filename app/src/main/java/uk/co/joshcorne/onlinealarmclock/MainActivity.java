ccfdaff9f2b2595ed53be4286529d8a2e7c9fdd2
Josh Corne <josh@joshcorne.co.uk>
2017-05-06 00:00:18 +0100

    Added features and fixed stuff

--- app/src/main/java/uk/co/joshcorne/onlinealarmclock/MainActivity.java
+++ app/src/main/java/uk/co/joshcorne/onlinealarmclock/MainActivity.java
@@ -5,14 +5,16 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import java.util.Calendar;
+
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -29,7 +31,12 @@ public class MainActivity extends AppCompatActivity
     AlarmManager alarmManager;
     Intent intent;
     PendingIntent pendingIntent;
-    final static String VIDEO_TAG = "video";
+
+    protected final static String VIDEO_TAG = "video";
+    protected final static String HOUR_TAG = "hour";
+    protected final static String MINUTE_TAG = "minute";
+    protected final static String STATUS_TAG = "status";
+    private final static String YOUTUBE_PACKAGE_NAME = "com.google.android.youtube";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,35 +46,29 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        setTitle("Youtube Alarm Clock");
+        setTitle(getString(R.string.title));
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor edit = sp.edit();
+        final SharedPreferences.Editor edit = sp.edit();
 
-        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
-        Switch statusSwitch = (Switch) findViewById(R.id.switch1);
+        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        Switch statusSwitch = (Switch) findViewById(R.id.statusSwitch);
         TextView videoTitle = (TextView) findViewById(R.id.textView4);
+
         String video = getIntent().getStringExtra(Intent.EXTRA_TEXT);
 
         //Get alarm deets
-        int hour = PreferenceManager.getDefaultSharedPreferences(this).getInt("hour", 0);
-        int minute = PreferenceManager.getDefaultSharedPreferences(this).getInt("minute", 0);
-        boolean status = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("status", false);
-        String videoDetails = PreferenceManager.getDefaultSharedPreferences(this).getString("video", "No video selected.");
+        int hour = PreferenceManager.getDefaultSharedPreferences(this).getInt(HOUR_TAG, 0);
+        int minute = PreferenceManager.getDefaultSharedPreferences(this).getInt(MINUTE_TAG, 0);
+        boolean status = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(STATUS_TAG, false);
+        String videoDetails = PreferenceManager.getDefaultSharedPreferences(this).getString(VIDEO_TAG, null);
 
-        //Create
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        if(video != null)
-        {
-            intent = new Intent(MainActivity.this, AlarmReceiver.class);
-            intent.putExtra(VIDEO_TAG, video);
-            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
-            edit.putString("video", video);
-        }
-
-        //Set up GUI
         statusSwitch.setChecked(status);
-        videoTitle.setText(videoDetails);
+        //Set up GUI
+        if(videoDetails != null)
+        {
+            videoTitle.setText(videoDetails);
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
         {
             timePicker.setHour(hour);
@@ -78,6 +79,31 @@ public class MainActivity extends AppCompatActivity
             timePicker.setCurrentHour(hour);
             timePicker.setCurrentMinute(minute);
         }
+
+        //Create
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        if(video != null)
+        {
+            intent = new Intent(MainActivity.this, AlarmReceiver.class);
+            intent.putExtra(VIDEO_TAG, video);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+            edit.putString(VIDEO_TAG, video);
+            statusSwitch.setChecked(true);
+            videoTitle.setText(video);
+        }
+
+        //Every change is saved
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+        {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute)
+            {
+                edit.putInt(HOUR_TAG, hourOfDay);
+                edit.putInt(MINUTE_TAG, minute);
+
+                edit.apply();
+            }
+        });
 
         //Status click handler
         statusSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
@@ -94,6 +120,7 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
+
         edit.apply();
     }
 
@@ -126,38 +153,48 @@ public class MainActivity extends AppCompatActivity
     {
         TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
 
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor edit = sp.edit();
-
+        //Get deets
         Calendar calendar = Calendar.getInstance();
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N)
         {
             calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
             calendar.set(Calendar.MINUTE, timePicker.getMinute());
-            edit.putInt("hour", timePicker.getHour());
-            edit.putInt("minute", timePicker.getMinute());
         }
         else
         {
             calendar.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
             calendar.set(Calendar.MINUTE, timePicker.getCurrentMinute());
-            edit.putInt("hour", timePicker.getCurrentHour());
-            edit.putInt("minute", timePicker.getCurrentMinute());
         }
 
+        //Set
         if(pendingIntent != null)
         {
-            edit.apply();
             alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
         }
         else
         {
-            Toast.makeText(this, "You haven't set a video.", Toast.LENGTH_SHORT).show();
+            //#Fail
+            Toast.makeText(this, R.string.no_video_set_toast, Toast.LENGTH_SHORT).show();
         }
     }
 
     private void disableAlarm()
     {
         alarmManager.cancel(pendingIntent);
+    }
+
+    public void openYoutube(View view)
+    {
+        String video = PreferenceManager.getDefaultSharedPreferences(this).getString(VIDEO_TAG, null);
+        if(video == null)
+        {
+            //Open youtube homepage if nothing set
+            startActivity(getPackageManager().getLaunchIntentForPackage(YOUTUBE_PACKAGE_NAME));
+        }
+        else
+        {
+            //Open video if set
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(video)));
+        }
     }
 }
